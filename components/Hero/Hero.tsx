e7e950ebda5ca84d7790d36312521cf34658467c
viktorhollanders <viktorhollanders@hey.com>
2025-11-16 20:57:09 +0000

    Fix button positoin on hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -9,12 +9,14 @@ export default function HeroIntro({ onStart }: { onStart: () => void }) {
     <div className={styles.hero}>
       <h1>Welcom to the grate wilderness!</h1>
       <p>Do you hear them all the aniamls</p>
-      <button type="button" className={styles.button} onClick={onStart}>
-        click here to Start Drawing or whatever
-      </button>
-      <Link href="/gallery" className={styles.button}>
-        View Gallery
-      </Link>
+      <div className={styles.button_container}>
+        <button type="button" className={styles.button} onClick={onStart}>
+          click here to Start Drawing or whatever
+        </button>
+        <Link href="/gallery" className={styles.button}>
+          View Gallery
+        </Link>
+      </div>
     </div>
   );
 }
